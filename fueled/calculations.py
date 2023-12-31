2df58fe5fc5a7829c5959b08ad48fdc20613b95f
shoshone-s <shoshone.y.smith@gmail.com>
2023-12-31 17:57:10 -0300

    updates to class methods for calculating fuel consumption

--- fueled/calculations.py
+++ fueled/calculations.py
@@ -7,13 +7,18 @@ from utils import Vehicle, Trip
 from pprint import pprint
 
 car1 = Vehicle(2000, 'Ford', 'e150')
-trip1 = Trip(120, 'miles')
+trip1 = Trip(135, 'ki')
 
-pprint(car1.specs())
 
+trip_calc = Vehicle.fuelConsumption(car1, trip1)
+
+for record in trip_calc:
+    pprint(record['est_consumption_gal'])
+# print(trip1)
 # kilometers = trip1.milesToKilometers()
 # car_info=car1.specs()
 # print(car1)
+# print(trip1.units)
 # print(kilometers)
 # pprint(car_info)
 # vehicle fuel mileage
